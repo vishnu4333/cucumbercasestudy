e341209b96befa67f45624d28762f653a0fed31e
vishnu4333 <vishnuedathottathil7@gmail.com>
2022-11-07 23:36:43 +0530

    first commit

--- cucumbercasestudy/src/test/java/runner/test_runner.java
+++ cucumbercasestudy/src/test/java/runner/test_runner.java
@@ -1,10 +1,14 @@
 package runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
 @CucumberOptions(features= {"src/test/resources/login/login.feature"},glue={"definition"},tags="@PositiveTesting",plugin= {"html:vishnu/htmlreport.html"})
 
-public class test_runner extends AbstractTestNGCucumberTests{
+public class test_runner{
 
 }
